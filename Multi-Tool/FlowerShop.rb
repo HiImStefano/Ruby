1b3470396087c2b10598ae6933c05395b0ea9ac3
Stefano <stefanovisentin436@yahoo.com>
2025-02-26 06:29:50 -0500

    Added more to Flower File

--- Multi-Tool/FlowerShop.rb
+++ Multi-Tool/FlowerShop.rb
@@ -1,11 +1,24 @@
 class Order
-    
+  def initialize(name)
+    @name = name
+  end
+
+
 end
 
 def FlowerShop
   puts "Welcome to the Flower Shop"
   puts "1. Create Order"
   puts "2. View Flower Prices"
-  puts "3. Exist"
-  
+  puts "3. Exit"
+  print "What would you like to do: "
+  customerchoice = gets.chomp
+
+  while customerchoice != 3
+    if customerchoice == 1
+      print "Can I get a name for the order: "
+      customername = gets.chomp
+      customerOrder = Order.new(customername)
+    end
+  end
 end
