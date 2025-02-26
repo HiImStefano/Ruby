3c91075f654981d2e8c2cc030c3d2995cb252222
Stefano <stefanovisentin436@yahoo.com>
2025-02-25 19:53:31 -0500

    Added in flower file

---
+++ Multi-Tool/FlowerShop.rb
@@ -0,0 +1,11 @@
+class Order
+    
+end
+
+def FlowerShop
+  puts "Welcome to the Flower Shop"
+  puts "1. Create Order"
+  puts "2. View Flower Prices"
+  puts "3. Exist"
+  
+end
--- Multi-Tool/MultiTool.rb
+++ Multi-Tool/MultiTool.rb
@@ -1,1 +1,18 @@
+
+def tool_input(choice)
+    case choice
+    when 'Flower'
+      load 'FlowerShop.rb'
+      flower
+    else
+      puts 'Not a choice'
+    end
+end
+
+
 puts "Welcome to the Multi-Tool! What would you like to do: "
+tool_choice = gets.chomp()
+while tool_choice != 'Exit'
+  tool_input(tool_choice)
+  tool_choice = gets.chomp()
+end
