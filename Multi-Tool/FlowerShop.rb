e81b9ae7a86b7635c1ecc057e3723fb2adc30811
Stefano Visentin <stefanovisentin436@yahoo.com>
2025-03-09 17:02:03 -0400

    Added in more code

--- Multi-Tool/FlowerShop.rb
+++ Multi-Tool/FlowerShop.rb
@@ -3,13 +3,38 @@ module FlowerPrices
   SUNFLOWERPRICE = 1.00
   LAVENDERPRICE = 2.40
   BABYSBREATHEPRICE = 0.75
+  ROSE = 'Rose'
+  SUNFLOWER = 'Sunflower'
+  LAVENDER = 'Lavender'
+  BABYSBREATHE = 'Babys Breathe'
 end
 
 class Order
   def initialize(name)
     @name = name
+    @cost = 0.00
+    @vase = []
   end
 
+  def addFlower(choice, customerOrderObj)
+    if choice == '1'
+      @vase.push(FlowerPrices::ROSE)
+      @cost += FlowerPrices::ROSEPRICE
+      puts "Rose Added"
+    elsif choice == '2'
+      @vase.push(FlowerPrices::SUNFLOWER)
+      @cost += FlowerPrices::SUNFLOWERPRICE
+    elsif choice == '3'
+      @vase.push(FlowerPrices::LAVENDER)
+      @cost += FlowerPrices::LAVENDERPRICE
+    elsif choice == '4'
+      @vase.push(FlowerPrices::BABYSBREATHE)
+      @cost += FlowerPrices::BABYSBREATHEPRICE
+    else
+      return "Not a Choice"
+    end
+
+  end
 
 end
 
@@ -31,9 +56,21 @@ def flowerShop
       customerOrderChoice = '0'
         puts "1. Add Flower"
         puts "2. View Order Price"
-        puts 
+        puts "3. Check Out"
         print "What would like to do with your order: "
-      
+        customerOrderChoice = gets.chomp
+        while customerOrderChoice != '3'
+          flowerChoice = '0'
+          if customerOrderChoice = '1'
+            puts "1. Rose: #{FlowerPrices::ROSEPRICE}"
+            puts "2. Sunflower: #{FlowerPrices::SUNFLOWERPRICE}"
+            puts "3. Lavender: #{FlowerPrices::LAVENDERPRICE}"
+            puts "4. Babys-Breathe: #{FlowerPrices::BABYSBREATHEPRICE}"
+            print "What Flower would you like to add: "
+            flowerChoice = gets.chomp
+            addFlower(flowerChoice, customerOrder)
+          end0
+        end
     elsif customerchoice == '2'
       puts "Price per flower: "
       puts "Rose: #{FlowerPrices::ROSEPRICE}"
